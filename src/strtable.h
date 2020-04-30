bbc253348ba4a19bd0988b17d602d8113a29eca7
KeyWorksRW <42786259+KeyWorksRW@users.noreply.github.com>
2020-04-30 11:49:15 -0700

    Remove strings that aren't being used
    The set of strings was initially a boilerplate of common strings. That no
    longer makes sense in an open-source project where localization is a
    possibility.

--- src/strtable.h
+++ src/strtable.h
@@ -6,21 +6,12 @@
 #ifdef _WIN32
 // #if 0  // comment out above and uncomment this to test non-Windows builds on a Windows platform
 
-    #define IDS_OOM                512  // ""There is not enough memory available for this task."
-    #define IDS_CANNOT_OPEN        513  // "Cannot open %ks"
-    #define IDS_INTERNAL_ERROR     514  // "An internal error has occurred."
-    #define IDS_FILE_MAPPING_ERROR 515  // "Unable to create file mapping memory for messages"
-    #define IDS_WRITE_ERROR        516  // "An error occurred while writing to \042%s\042."
-    #define IDS_FILE_SAVED         517  // "Contents saved to "
+    #define IDS_FILE_MAPPING_ERROR 1024  // "Unable to create file mapping memory for messages"
+    #define IDS_FILE_SAVED         1025  // "Contents saved to "
 
 #else
 
-constexpr auto IDS_OOM = "There is not enough memory available for this task.\nQuit one or more applications to increase available "
-                         "constexpr auto memory, and then try again.";
-constexpr auto IDS_CANNOT_OPEN = "Cannot open %ks";
-constexpr auto IDS_INTERNAL_ERROR = "An internal error has occurred.";
 constexpr auto IDS_FILE_MAPPING_ERROR = "Unable to create file mapping memory for messages";
-constexpr auto IDS_WRITE_ERROR = "An error occurred while writing to \042%s\042.";
 constexpr auto IDS_FILE_SAVED = "Contents saved to ";
 
 #endif
